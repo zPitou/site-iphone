705ce97f12fa4c3751a9062847f513f93d4ef8d9
Nicolas <nicolas.aragao.g@gmail.com>
2025-05-27 19:15:36 -0300

    Primeiro commit

---
+++ plugins/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $("#banners ul").bxSlider({
+        auto: true,
+        speed: 1000,
+        // mode: 'fade'
+        // pager: false
+    });
+
+    $('#fotos #galeria').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
